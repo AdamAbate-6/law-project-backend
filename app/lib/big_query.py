4795f4cc00d201a4c6dd847fe2c3e94e3bcb84a2
Adam Abate <abateadam6@gmail.com>
2023-08-15 07:32:29 -0400

    Big Query utils changes for PyLint

--- app/lib/big_query.py
+++ app/lib/big_query.py
@@ -7,12 +7,15 @@ def query_patent(patent_spif: str) -> tuple[dict, bool]:
         "GOOGLE_APPLICATION_CREDENTIALS"
     ] = "../law-project-service-account.json"
     client = bigquery.Client()
-
-    # Perform a query. Need to UNNEST the struct of string arrays in several fields.
+    # Perform a query. Need to UNNEST the struct of string arrays in several
+    #  fields.
     # TODO Validate query so we don't get an injection attack.
     QUERY = (
-        f"SELECT spif_publication_number as spif, t.text as title,  a.text as abstract, c.text as claims "
-        f"FROM `patents-public-data.patents.publications`, UNNEST(title_localized) as t, UNNEST(abstract_localized) as a,  UNNEST(claims_localized) as c "
+        f"SELECT spif_publication_number as spif, t.text as title, "
+        f"a.text as abstract, c.text as claims "
+        f"FROM `patents-public-data.patents.publications`, "
+        f"UNNEST(title_localized) as t, UNNEST(abstract_localized) as a, "
+        f"UNNEST(claims_localized) as c "
         f'WHERE spif_publication_number = "{patent_spif}" '
         f"LIMIT 100"
     )
@@ -20,7 +23,8 @@ def query_patent(patent_spif: str) -> tuple[dict, bool]:
     query_job = client.query(QUERY)  # Send API request.
     rows = query_job.result()  # Waits for query to finish.
 
-    # `rows` is an iterator, but SPIF should be unique to one patent, so we should only iterate once.
+    # `rows` is an iterator, but SPIF should be unique to one patent, so we
+    #  should only iterate once.
     num_iters = 0
     patent_data = dict()
     for row in rows:
@@ -33,7 +37,8 @@ def query_patent(patent_spif: str) -> tuple[dict, bool]:
         num_iters += 1
         assert (
             num_iters == 1
-        ), f"More than one entry was returned from BigQuery query to patent SPIF {patent_spif}; that cannot be correct."
+        ), f"More than one entry was returned from BigQuery query to "
+        f"patent SPIF {patent_spif}; that cannot be correct."
 
     found_patent_in_bq = True if len(patent_data) > 1 else False
     return patent_data, found_patent_in_bq
