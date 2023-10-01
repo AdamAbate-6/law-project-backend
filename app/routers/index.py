b3dcee61d07aaae494e97d111a11e0784138dffb
Adam Abate <abateadam6@gmail.com>
2023-10-01 18:55:59 -0400

    Fix bug of loaded index not working due to a missing docstore

--- app/routers/index.py
+++ app/routers/index.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, HTTPException, Response, status
 from llama_index import StorageContext
 from llama_index.storage.index_store import MongoIndexStore
+from llama_index.storage.docstore import MongoDocumentStore
 
 from lib.models.patent import PatentDataToClient
 from lib.database import reformat_mongodb_id_field, fetch_one_patent, db_uri
@@ -27,11 +28,19 @@ async def post_patent_index(
     if db_response:
         patent_dict = {k: v for k, v in db_response.items() if k != "_id"}
         patent_nodes = get_patent_nodes(patent_dict, service_context)
+
         # Index will be persisted to a DB named law_patent_indices in a
         #  collection whose name is the patent_spif.
         index_store = MongoIndexStore.from_uri(
             uri=db_uri, db_name="law_patent_indices", namespace=patent_spif
         )
+        # Likewise, have to persist docs. Otherwise, querying a loaded index
+        #  as a query engine will raise an error that a doc_id is not found.
+        doc_store = MongoDocumentStore.from_uri(
+            uri=db_uri, db_name="law_patent_nodes", namespace=patent_spif
+        )
+        doc_store.add_documents(patent_nodes)
+
         # NOTE: Specifying storage_context as a MongoIndexStore should
         #  automatically persist the index to MongoDB.
         custom_index = CustomKeywordTableIndex(
@@ -39,13 +48,18 @@ async def post_patent_index(
             service_context=service_context,
             keyword_extract_template=extract_keyword_prompt,
             max_keywords_per_chunk=1,
+            docstore=doc_store,
             storage_context=StorageContext.from_defaults(
-                index_store=index_store
+                index_store=index_store, docstore=doc_store
             ),
         )
-        custom_index.summary = (
-            f"Use this index for queries about patent with SPIF {patent_spif}"
-        )
+        # NOTE: Commenting out the below because it's unnecessary DB I/O bits.
+        #  Can just construct the summary out of patent_spif when the sqqe is
+        #  created.
+        # custom_index.summary = (
+        #     f"Use this index for queries about patent with SPIF {patent_spif}"
+        # )
+        # custom_index.storage_context.persist()
         api_response.status_code = status.HTTP_200_OK
         return reformat_mongodb_id_field(db_response)
     else:
